ca707ab3e8308b005b15167457ee2fe8f37a20ac
s0me0nec <s2021151432@firstasia.edu.ph>
2026-02-08 11:26:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let noBtn = document.getElementById("noBtn");
 let yesBtn = document.getElementById("yesBtn");
+let heartsContainer = document.getElementById("hearts");
 
+/* Make NO button run */
 noBtn.addEventListener("mouseover", () => {
     let x = Math.random() * 80 + "vw";
     let y = Math.random() * 80 + "vh";
@@ -9,6 +11,26 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.top = y;
 });
 
+/* Go to second page after YES */
 yesBtn.addEventListener("click", () => {
     window.location.href = "valentine.html";
 });
+
+/* falling hearts generator */
+function createHeart() {
+    let heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "💗";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+    heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
