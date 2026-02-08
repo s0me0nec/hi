11173636f979dfe2a044e27459af9a5e88709b7f
s0me0nec <s2021151432@firstasia.edu.ph>
2026-02-08 11:16:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const noBtn = document.getElementById("noBtn");
-const yesBtn = document.getElementById("yesBtn");
+let noBtn = document.getElementById("noBtn");
+let yesBtn = document.getElementById("yesBtn");
 
 noBtn.addEventListener("mouseover", () => {
-    const x = Math.random() * (window.innerWidth - 100);
-    const y = Math.random() * (window.innerHeight - 100);
-
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
+    let x = Math.random() * 80 + "vw";
+    let y = Math.random() * 80 + "vh";
+    noBtn.style.position = "absolute";
+    noBtn.style.left = x;
+    noBtn.style.top = y;
 });
 
-// When she clicks YES → go to the message page
 yesBtn.addEventListener("click", () => {
     window.location.href = "valentine.html";
 });
